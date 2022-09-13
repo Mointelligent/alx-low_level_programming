7ce306b29270a0cdc8238e123fca95c13be7d667
Demmythetechie <demmythetechie@gmail.com>
2022-09-13 04:59:41 +0000

    Task 5 Done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints a sentence(Formatted string) using printf and returns 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
