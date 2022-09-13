bb9d61670ae83956d5a88bab132fcf51533b5c5e
Demmythetechie <demmythetechie@gmail.com>
2022-09-13 04:56:43 +0000

    Task 4 Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *main - prints a sentence using puts function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
+	
+	puts(mystr);
+	return (0);
+}
